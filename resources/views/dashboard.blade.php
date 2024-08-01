11e04b49ad3b99e42fe1c12208ec09296fad4040
bayutriprabowo <bay.tri@gmail.com>
2024-08-01 09:24:21 +0700

    menambahkan menu master user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,6 +22,18 @@
                     <!-- Content ke kanan dengan row -->
                     <div class="row">
                         <!-- isi pokok content-->
+                        @if (auth()->user()->masterRole->name == 'superuser')
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">User</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="/users">View
+                                            Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
                         <div class="col-xl-3 col-md-6">
                             <div class="card bg-primary text-white mb-4">
                                 <div class="card-body">Master 1</div>
